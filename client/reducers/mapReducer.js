d8a233e8e2c512d7f0b3120777c9d4d029310023
Ruzeb <ruzeb1996@gmail.com>
2022-01-06 15:01:45 -0500

    added mapReducer test

--- client/reducers/mapReducer.js
+++ client/reducers/mapReducer.js
@@ -2,15 +2,26 @@ import * as types from '../constants/actionTypes';
 import axios from 'axios';
 import { defaultMaxListeners } from 'events';
 
-const allIncidents = [];
-axios.get(`api/incidents`)
-.then(({data}) => {
-    //console.log('data', data);
-    for(let incident of data){
-        allIncidents.push(incident)
-    }
-})
-.catch(console.error);
+// const allIncidents = [];
+// axios.get(`api/incidents`)
+// .then(({data}) => {
+//     //console.log('data', data);
+//     for(let incident of data){
+//         allIncidents.push(incident)
+//     }
+// })
+// .catch(console.error);
+const allIncidents= [{
+  details: "something is afoul",
+  image_url: "www.png",
+  incident_id: "5",
+  location_id: null,
+  street_name: "163 Branham Ln, San Jose, CA 95136",
+  time: "1/3/2022, 8:57:30 AM",
+  title: "Burger robber at Mcdonald's!",
+  user_id: null,
+  video_url: "",
+}]
 
 const initialState = {
   pinLocations: [],
